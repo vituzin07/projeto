34400c5f5e349b8c9ea8f6991a00bf3be3add680
Joâo victor <jv2644099@gmail.com>
2024-08-28 16:40:32 -0300

    Feito

--- src/App.js
+++ src/App.js
@@ -4,7 +4,18 @@ import Card from './Card/Card.js';
 function App() {
   return (
     <div className="App">
-      <Card texto = "jogo"/>
+      <Card 
+      imagem='foto-capa-pokmfr.png'
+      titulo='Pokémon Fire Red'
+      desenvolvedor='Game Freak'
+      plataforma= 'Game Boy Advance'
+      />
+      <Card
+     imagem='foto-capa-hades.png'
+     titulo='Hades'
+     desenvolvedor='Supergiant Games'
+     plataforma= 'Nintendo Switch, Playstation 5, Playstatio 4, macOS, iOS, Xbox One, Xbox Series X e Series S, Microsoft Windows'
+      />
     </div>
   );
 }
--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,8 +1,35 @@
 import './Card.css';
 
-export default function Card(props) {
+export default function Card({imagem, titulo, desenvolvedor, plataforma }) {
 
-    return  <Card>
-    {props.texto}
-    </Card>
+    return(
+        <div className='container'>
+            <div className = 'Imagem'>
+                <img src = {imagem} alt = 'Foto do jogo'></img>
+            </div>
+            <div className='Titulo'>
+                <h1>
+                    {titulo}
+                </h1>
+            </div>
+            <div className='Desenvolvedor'>
+                <h2>
+                    {desenvolvedor}
+                </h2>
+            </div>
+            <div className='Plataforma'>
+                <h2>
+                    {plataforma}
+                </h2>
+
+            </div>
+
+     
+
+       </div>
+
+  
+
+   
+    )
 }
